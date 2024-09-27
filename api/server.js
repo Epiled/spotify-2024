5de19c7d695eab7731883f78ac446424280eaaf5
Felipe De Andrade <felipe.deam98@gmail.com>
2024-09-27 18:36:24 -0300

    fix: Response of API

--- api/server.js
+++ api/server.js
@@ -1,13 +1,18 @@
-import jsonServer from 'json-server';
-import cors from 'cors';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import jsonServer from "json-server";
+import cors from "cors";
 
 const server = jsonServer.create();
-const router = jsonServer.router('./db/playlist.json');
+const router = jsonServer.router('../db/playlist.json');
 const middlewares = jsonServer.defaults();
 
 server.use(cors()); // Adiciona suporte a CORS
 server.use(middlewares);
 server.use(router);
+
+server.listen(3000, () => {
+  console.log("Okay");
+})
 
 export default (req, res) => {
   server(req, res);
