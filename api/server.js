ae5a2b53d98c7b33949cc31d1a2f78015042b24a
Felipe De Andrade <felipe.deam98@gmail.com>
2024-09-27 19:07:33 -0300

    fix: API

--- api/server.js
+++ api/server.js
@@ -1,14 +1,8 @@
 import jsonServer from "json-server";
 import cors from "cors";
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-// Define manualmente __dirname usando import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router(join(__dirname, '../db/playlist.json')); // Caminho absoluto para o JSON
+const router = jsonServer.router('./db/playlist.json'); // Caminho absoluto para o JSON
 const middlewares = jsonServer.defaults();
 
 server.use(cors()); // Adiciona suporte a CORS
